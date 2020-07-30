3ec3087f428416e9ca1abc822431e10515c8d1f8
Mariana Silva <lullabyh@users.noreply.github.com>
2020-07-29 22:48:26 -0300

    Criando rotaas iniciais

---
+++ index.js
@@ -0,0 +1,23 @@
+//importa express
+const express  =  require("express");
+//importa BodyParser
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.send('Olá Mundo');
+})
+
+app.post('/mensagem', (req, res) => {
+    console.log(req)
+    res.send("mensagem recebida." + req.body.msg);
+})
+
+
+//GERAR UMA PORTA EM QUE IRÁ RODAR O NODE
+const porta = process.env.PORT || 3000;
+
+app.listen(porta,'127.0.0.1', () =>{
+    console.log('Servidor node rodando!');
+} )
